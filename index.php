273dd894ac965eca453f30a9d3d89c86edf01fbc
Colin von Heuring <colinvh@divitu.com>
2019-04-07 16:13:32 -0600

    ...

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?
-require_once 'lib/db.inc.php';
 require_once 'lib/session.inc.php';
+require_once 'lib/db.inc.php';
 require_once 'lib/site.inc.php';
 
 $user = User::require_login();
@@ -9,5 +9,11 @@ if ($_SERVER['PATH_INFO']) {
 	$game = Game::from_path($_SERVER['PATH_INFO']);
 	$sub = Subject::from_path($_SERVER['PATH_INFO']);
 }
+
+ob_start();
+?>
+HOME
+<?
+page([], head(), pheader(), ob_get_clean(), pfooter());
 
 ?>
